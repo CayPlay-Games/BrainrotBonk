509ede00a62bbcfc2427573fb41908afaa021695
Justin <jkellyprogramming@gmail.com>
2026-02-18 14:17:42 -0500

    adding some titles

--- src/Configs/TitlesConfig.lua
+++ src/Configs/TitlesConfig.lua
@@ -39,5 +39,47 @@ return TableHelper:DeepFreeze({
 			Description = "Join the game group!",
 			Color = Color3.fromRGB(0, 200, 255),
 		},
+
+		-- Rank tier titles
+		Bronze = {
+			DisplayName = "Bronze",
+			Description = "Reached Bronze rank",
+			Color = Color3.fromRGB(205, 127, 50),
+		},
+		Silver = {
+			DisplayName = "Silver",
+			Description = "Reached Silver rank",
+			Color = Color3.fromRGB(192, 192, 200),
+		},
+		Gold = {
+			DisplayName = "Gold",
+			Description = "Reached Gold rank",
+			Color = Color3.fromRGB(255, 215, 0),
+		},
+		Platinum = {
+			DisplayName = "Platinum",
+			Description = "Reached Platinum rank",
+			Color = Color3.fromRGB(150, 200, 220),
+		},
+		Emerald = {
+			DisplayName = "Emerald",
+			Description = "Reached Emerald rank",
+			Color = Color3.fromRGB(80, 200, 120),
+		},
+		Diamond = {
+			DisplayName = "Diamond",
+			Description = "Reached Diamond rank",
+			Color = Color3.fromRGB(180, 230, 255),
+		},
+		Master = {
+			DisplayName = "Master",
+			Description = "Reached Master rank",
+			Color = Color3.fromRGB(180, 100, 220),
+		},
+		Grandmaster = {
+			DisplayName = "Grandmaster",
+			Description = "Reached Grandmaster rank",
+			Color = Color3.fromRGB(200, 50, 50),
+		},
 	},
 })
